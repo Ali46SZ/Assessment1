47278d7a760e22102ac99da3748c43819fa17259
Ali <alisomayh0@gmail.com>
2023-08-11 19:12:30 +0300

    Refactor method name

--- src/main/java/org/stc/Main.java
+++ src/main/java/org/stc/Main.java
@@ -2,33 +2,30 @@ package org.stc;
 
 public class Main {
     public static void main(String[] args) {
-        String s = "abd(jnb)asdf";
-        String s2 = "abdjnbasdf";
-        String s3 = "dd(df)a(ghhh)";
-        System.out.println(reverseSubStrings(s));
-        System.out.println(reverseSubStrings(s2));
-        System.out.println(reverseSubStrings(s3));
+        System.out.println(reverseInParenthesesStrings("abd(jnb)asdf"));
+        System.out.println(reverseInParenthesesStrings("abdjnbasdf"));
+        System.out.println(reverseInParenthesesStrings("dd(df)a(ghhh)"));
     }
 
-    public static String reverseSubStrings(String str) {
+    public static String reverseInParenthesesStrings(String str) {
         StringBuilder finalString = new StringBuilder();
         StringBuilder subString = new StringBuilder();
-        boolean add = false;
+        boolean isInParentheses = false;
         for (char ch : str.toCharArray()) {
-            if (add == true && ch != ')') {
+            if (isInParentheses && ch != ')') {
                 subString.append(ch);
                 continue;
             }
             if (ch == '(') {
                 finalString.append(ch);
-                add = true;
+                isInParentheses = true;
                 continue;
             }
             if (ch == ')') {
                 finalString.append(subString.reverse());
                 finalString.append(ch);
                 subString.setLength(0);
-                add = false;
+                isInParentheses = false;
                 continue;
             }
             finalString.append(ch);
